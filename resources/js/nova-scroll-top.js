31160817967a69ff66b16b0757e02c2bdcd05870
Muhittin Tan <muhittin.tan@boun.edu.tr>
2019-03-22 10:52:55 +0300

    working

--- resources/js/nova-scroll-top.js
+++ resources/js/nova-scroll-top.js
@@ -1,12 +1,10 @@
 import animateScrollTo from 'animated-scroll-to';
 
 Nova.booting((Vue, router) => {
-    Nova.booting((Vue, router) => {
-        router.beforeEach(function (to, from, next) {
-            setTimeout(() => {
-                animateScrollTo(0);
-            }, 100);
-            next();
-        });
+    router.beforeEach(function (to, from, next) {
+        setTimeout(() => {
+            animateScrollTo(0);
+        }, 100);
+        next();
     });
 });
