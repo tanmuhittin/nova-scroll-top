3525bc4c475caf72673be6f9b8def0760c885408
Muhittin Tan <muhittin.tan@boun.edu.tr>
2019-04-19 16:31:44 +0300

    issue #1 prevent scroll behaviour if modal is active

--- resources/js/nova-scroll-to-error.js
+++ resources/js/nova-scroll-to-error.js
@@ -5,11 +5,12 @@ import xhook from 'xhook'
 Nova.booting(() => {
     xhook.after(function(request, response) {
         if(response.status === 422){
-            console.log("validation error");
-            setTimeout(() => {
-                if(document.querySelector('.border-danger'))
-                    animateScrollTo(document.querySelector('.border-danger'))
-            }, 200);
+            if(document.getElementsByClassName('modal').length < 1) {
+                setTimeout(() => {
+                    if (document.querySelector('.border-danger'))
+                        animateScrollTo(document.querySelector('.border-danger'))
+                }, 200);
+            }
         }
     });
 });
