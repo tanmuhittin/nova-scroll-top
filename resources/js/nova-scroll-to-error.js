31015bacdabb58f2cb6c6706c69eae12d68d04cf
David Cox <dave.cox@dunntire.com>
2020-05-11 20:19:14 -0400

    retarget scroll for better UI and fix checkbox validation error handling

--- resources/js/nova-scroll-to-error.js
+++ resources/js/nova-scroll-to-error.js
@@ -6,8 +6,8 @@ Nova.booting(() => {
         this.addEventListener('load', function() {
             if (this.readyState === 4 && this.status === 422 && document.getElementsByClassName('modal').length < 1) {
                 setTimeout(() => {
-                    if (document.querySelector('.border-danger'))
-                        animateScrollTo(document.querySelector('.border-danger'))
+                    if (document.querySelector('.error-text'))
+                        animateScrollTo(document.querySelector('.error-text').parentNode)
                 }, 200);
             }
         });
