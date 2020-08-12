e7a2b8016175e31530144109f01343f1271c485b
sergey <sergey@vrpo.ru>
2020-08-12 18:02:55 +0300

    Check changes of route for scroll

--- resources/js/nova-scroll-top.js
+++ resources/js/nova-scroll-top.js
@@ -3,9 +3,7 @@ import animateScrollTo from 'animated-scroll-to';
 Nova.booting((Vue, router) => {
     router.beforeEach(function (to, from, next) {
         setTimeout(() => {
-            if(typeof document.activeElement.type !== 'undefined' && document.activeElement.closest('div[dusk*=index-component]')){
-                animateScrollTo(document.activeElement.closest('div[dusk*=index-component]').offsetTop);
-            }else{
+            if(to.path != from.path) {
                 animateScrollTo(0);
             }
         }, 100);
